a183351074ce2bd1dc2a0c39db216792f449f19f
Premnath Sah T.H <prem.sah@gmail.com>
2020-12-12 20:06:15 +0530

    Parsing the html

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { fetchWeather } from './fetch-weather'
+import { parse } from 'node-html-parser';
 
 
 addEventListener('fetch', (event: FetchEvent) => {
@@ -6,13 +7,10 @@ addEventListener('fetch', (event: FetchEvent) => {
 })
 
 async function handleRequest(request: Request) {
-  let test = ['BAJAJ-AUTO', 'DRREDDY']
-  for (var i = 0; i < test.length; i++) {
-    const weather = await fetchWeather(test[i]);
-    console.log(weather.text())
-    return weather;
-  }
-  //const body = `
+  let test = 'BAJAJ-AUTO'
+  const weather = await fetchWeather(test);
+  const root = parse(await weather.text());
+  return new Response(root.querySelector('.Fz\(36px\)').text);
   // ${weather.location.city}, ${weather.location.region}<br>
   //${weather.item.condition.temp} ${weather.item.condition.text}
   //`.trim()
